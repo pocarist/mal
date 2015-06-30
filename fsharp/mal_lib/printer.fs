578c97e36e3ed9db763f9c9064305ad453bf1bfc
toku <Kenji.Tokudome@grassvalley.com>
2015-06-30 18:16:38 +0900

    fix pr_str

--- fsharp/mal_lib/printer.fs
+++ fsharp/mal_lib/printer.fs
@@ -6,7 +6,7 @@ open Types
 type Printer() =
     static member pr_str (exp, ?sep, ?print_readably) =
         let sep = defaultArg sep " "
-        let print_readably = defaultArg print_readably true
+        let print_readably = defaultArg print_readably false
         let escape (x : string) = x.Replace("\\", "\\\\").Replace("\"", "\\\"").Replace("\n", "\\n")
         let rec f = function
             | Number x -> string x
